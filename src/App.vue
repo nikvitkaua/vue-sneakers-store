2e4e1a8af59d8298ce446ae91fb0fff6ef985d9a
Mykyta Paiuka <nikvitka.it@gmail.com>
2024-02-02 13:02:13 +0200

    create a structure for the project and basic layout

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+</script>
+
+<template>
+  <div 
+    class="bg-white w-4/5 h-screen m-auto rounded-xl shadow-xl mt-14"
+  >
+    <h1>some text</h1>
+  </div>
+</template>
+
+<style scoped>
+</style>
