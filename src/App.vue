b51f1478f6cce69757ea37858a8171868a80b92d
Mykyta Paiuka <nikvitka.it@gmail.com>
2024-02-03 11:19:07 +0200

    add sorting and search function

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { onMounted, ref } from 'vue';
+import { onMounted, ref, computed } from 'vue';
 import axios from 'axios';
 
 import MainHeader from './components/MainHeader.vue';
@@ -7,6 +7,8 @@ import CardList from './components/CardList.vue';
 import DrawerApp from './components/DrawerApp.vue';
 
 const items = ref([]);
+const selectedOption = ref('name');
+const searchQuery = ref('');
 
 onMounted(async () => {
   try {
@@ -18,7 +20,29 @@ onMounted(async () => {
   }
 });
 
-console.log(items)
+const filteredItems = computed(() => {
+  const sortedItems = [...items.value];
+
+  const sortByOption = (a, b) => {
+    if (selectedOption.value === "price-low") {
+      return a.price - b.price;
+    } else if (selectedOption.value === "price-high") {
+      return b.price - a.price;
+    } else {
+      return a.title.localeCompare(b.title);
+    }
+  };
+
+  sortedItems.sort(sortByOption);
+
+  return sortedItems.filter(item =>
+    item.title.toLowerCase().includes(searchQuery.value.toLowerCase())
+  );
+});
+
+const onChangeSelect = (event) => {
+  selectedOption.value = event.target.value;
+};
 
 </script>
 
@@ -33,17 +57,19 @@ console.log(items)
         <h2 class="text-3xl font-bold">All sneakers</h2>
 
         <div class="flex gap-4">
-          <select 
+          <select
+            @change="onChangeSelect" 
             class="py-2 px-2 border rounded-md outline-none cursor-pointer"
           >
-            <option>Name</option>
-            <option>Price (low)</option>
-            <option>Price (high)</option>
+            <option value="name">Name</option>
+            <option value="price-low">Price (low)</option>
+            <option value="price-high">Price (high)</option>
           </select>
 
           <div class="relative">
             <img class="absolute top-3 left-4" src="/search.svg" alt="search icon">
-            <input 
+            <input
+              v-model="searchQuery" 
               type="text"
               placeholder="Search..."
               class="border rounded-md py-2 pl-12 pr-4 outline-none focus:border-gray-400"
@@ -53,7 +79,7 @@ console.log(items)
       </div>
 
 
-      <CardList :items="items" />
+      <CardList :items="filteredItems" />
     </div>
   </div>
 </template>
